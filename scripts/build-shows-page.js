6acc1c67a238c20bba352f870f5079532bc370f5
Daniela Conde <daniela_conde2@hotmail.com>
2025-02-07 09:41:40 -0700

    add API to index and shows js code to replace local array

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,47 +1,56 @@
-const upcomingShows = [ 
-    {
-        date: "Mon Sept 09 2024",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA",
-    },
-    {
-        date: "Tue Sept 17 2024",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA",
-    },
-    {
-        date: "Sat Oct 12 2024",
-        venue: "View Lounge",
-        location: "San Francisco, CA",
-    },
-    {
-        date: "Sat Nov 16 2024",
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA",
-    },
-    {
-        date: "Fri Nov 29 2024",
-        venue: "Moscow Center",
-        location: "San Francisco, CA",
-    },
-    {
-        date: "Wed Dec 18 2024",
-        venue: "Press Club",
-        location: "San Francisco, CA",
-    },
-  ];
+// const upcomingShows = [ 
+//     {
+//         date: "Mon Sept 09 2024",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA",
+//     },
+//     {
+//         date: "Tue Sept 17 2024",
+//         venue: "Pier 3 East",
+//         location: "San Francisco, CA",
+//     },
+//     {
+//         date: "Sat Oct 12 2024",
+//         venue: "View Lounge",
+//         location: "San Francisco, CA",
+//     },
+//     {
+//         date: "Sat Nov 16 2024",
+//         venue: "Hyatt Agency",
+//         location: "San Francisco, CA",
+//     },
+//     {
+//         date: "Fri Nov 29 2024",
+//         venue: "Moscow Center",
+//         location: "San Francisco, CA",
+//     },
+//     {
+//         date: "Wed Dec 18 2024",
+//         venue: "Press Club",
+//         location: "San Francisco, CA",
+//     },
+//   ];
 
   // Get the container where you'll display the upcoming shows
 const showsList = document.querySelector(".shows-list"); // assuming an <ul> or <div> container for the shows
 
 // Function to loop through the upcoming shows and display them
-function loopAndAppendShows() {
+async function loopAndAppendShows() {
   showsList.innerHTML = ""; // clears the container before appending new content
+
+  const response = await axios.get("https://unit-2-project-api-25c1595833b2.herokuapp.com/showdates?api_key=daniela");
+  console.log(response.data);
+  const upcomingShows = response.data;
 
   for (let i = 0; i < upcomingShows.length; i++) {
     // Create a list item for each show
     const showItem = document.createElement("li");
     showItem.classList.add("show-item");
+
+    //convert timestamp to date in XX/XX/XXXX format
+    const date = new Date(upcomingShows[i].date);
+    console.log(date.toLocaleDateString('en-US'));
+    const dateNumber = date.toLocaleDateString('en-US')
 
     // DATE label
     const dateLabel = document.createElement("div");
@@ -51,7 +60,7 @@ function loopAndAppendShows() {
 
     // Create a heading for the show date
     const showDate = document.createElement("h3");
-    showDate.innerText = `Date: ${upcomingShows[i].date}`;
+    showDate.innerText = dateNumber;
     showDate.classList.add("show-date");
     showItem.appendChild(showDate);
 
@@ -63,7 +72,7 @@ function loopAndAppendShows() {
 
     // Create a paragraph for the venue
     const showVenue = document.createElement("p");
-    showVenue.innerText = `Venue: ${upcomingShows[i].venue}`;
+    showVenue.innerText = upcomingShows[i].place;
     showVenue.classList.add("show-venue");
     showItem.appendChild(showVenue);
 
@@ -75,7 +84,7 @@ function loopAndAppendShows() {
 
     // Create a paragraph for the location
     const showLocation = document.createElement("p");
-    showLocation.innerText = `Location: ${upcomingShows[i].location}`;
+    showLocation.innerText = upcomingShows[i].location;
     showLocation.classList.add("show-location");
     showItem.appendChild(showLocation);
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,32 +1,36 @@
-const bioComments = [ 
-    {
-      posted: "11/02/2023",
-      name: "Victor Pinto",
-      description:
-        "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    },
-    {
-      posted: "10/28/2023",
-      name: "Christina Cabrera",
-      description:
-        "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    },
-    {
-      posted: "10/20/2023",
-      name: "Isaac Tadesse",
-      description:
-        "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    },
-  ];
+// const bioComments = [ 
+//     {
+//       posted: "11/02/2023",
+//       name: "Victor Pinto",
+//       description:
+//         "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//     },
+//     {
+//       posted: "10/28/2023",
+//       name: "Christina Cabrera",
+//       description:
+//         "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//     },
+//     {
+//       posted: "10/20/2023",
+//       name: "Isaac Tadesse",
+//       description:
+//         "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//     },
+//   ];
   
   // get the form from the HTML file
   const commentForm = document.querySelector(".comment-form");
   // get the comment list from the HTML file
   const commentList = document.querySelector(".comment-list"); // <ul>
   
-  function loopAndAppendComments() {
+  async function loopAndAppendComments() {
     commentList.innerText = ""; // clears the <ul> before looping and appending, avoiding duplicates
   
+    const response = await axios.get("https://unit-2-project-api-25c1595833b2.herokuapp.com/comments?api_key=daniela");
+    console.log(response.data);
+    const bioComments = response.data;
+
     for (let i = 0; i < bioComments.length; i++) {
       // create a comment list item
       const commentListItem = document.createElement("li");
@@ -37,10 +41,21 @@ const bioComments = [
       commentAuthor.innerText = bioComments[i].name;
       commentAuthor.classList.add("comment-list__name");
       commentListItem.appendChild(commentAuthor); // append to the <li>
+
+      //convert timestamp to date in XX/XX/XXXX format
+      const date = new Date(bioComments[i].timestamp);
+      console.log(date.toLocaleDateString('en-US'));
+      const dateNumber = date.toLocaleDateString('en-US')
+
+      //create a p tag for the comment timestamp
+      const commentDate = document.createElement("p");
+      commentDate.innerText = dateNumber;
+      commentDate.classList.add("comment-list__date");
+      commentListItem.appendChild(commentDate); 
   
       // create a p tag for the comment body
       const commentBody = document.createElement("p");
-      commentBody.innerText = bioComments[i].description;
+      commentBody.innerText = bioComments[i].comment;
       commentBody.classList.add("comment-list__description");
       commentListItem.appendChild(commentBody);
   
